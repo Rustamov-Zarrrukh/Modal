e29d05383dc67eba0f037792791fa76a6e3f4dd8
Rustamov-Zarrrukh <zarruhzak@gmail.com>
2025-03-22 09:08:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let modal = document.querySelector(".modal")
+let showModal = document.querySelector(".showModal")
+let close = document.querySelector(".close")
+
+showModal.onclick = () => modal.showModal()
+close.onclick = () => modal.close()
